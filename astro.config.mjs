60d610d895bc7e2bdc82b13029f54fb80fed47ae
Max <mr.mvo14@gmail.com>
2026-06-19 14:44:52 -0700

    fix: configure Twitch embeds parent domains for production and previews

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://mundoapg.com",
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
